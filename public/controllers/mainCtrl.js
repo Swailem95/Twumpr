a533a85c26e8a0724c88df86de527bd918e57f25
Youssef Swailem <youssefswailem@gmail.com>
2016-11-28 17:38:15 +0200

    Added words, hashtags and mentions

--- public/controllers/mainCtrl.js
+++ public/controllers/mainCtrl.js
@@ -1,22 +1,23 @@
 app.controller('mainCtrl', function($http, $scope, $window){
 
-    // $window.onLoad = function(){
-    // $scope.getData = function(){
 
-        $http({
-            method: "GET",
-            url: "/getData"
-        }).then(function(response){
+    $scope.done = false;
 
-            // console.log(response);
+    $http({
+        method: "GET",
+        url: "/getData"
+    }).then(function(response){
 
-            $scope.words = response.data.words;
-            $scope.hashtags = response.data.hashtags;
-            $scope.mentions = response.data.mentions;
+        // console.log(response);
+
+        $scope.words = response.data.words;
+        $scope.hashtags = response.data.hashtags;
+        $scope.mentions = response.data.mentions;
+
+        $scope.done = true;
+
+    });
 
 
-        });
-
-    // }
 
 });
